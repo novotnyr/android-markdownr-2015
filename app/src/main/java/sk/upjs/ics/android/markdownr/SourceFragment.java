9a448309fd0c4a06c8d6c180a32570561c43842e
novotnyr <robert.novotny@upjs.sk>
2015-04-19 10:08:00 +0200

    add fragment for HTML/Markdown source

---
+++ app/src/main/java/sk/upjs/ics/android/markdownr/SourceFragment.java
@@ -0,0 +1,19 @@
+package sk.upjs.ics.android.markdownr;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SourceFragment extends Fragment {
+    public SourceFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_source, container, false);
+    }
+}
