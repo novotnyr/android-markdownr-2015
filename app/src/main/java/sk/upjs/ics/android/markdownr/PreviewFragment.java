93f81012f387ff7d87d8f67dc0f5c376cbb33e0b
novotnyr <robert.novotny@upjs.sk>
2015-04-19 17:28:07 +0200

    initialize fragment with sample HTML data

--- app/src/main/java/sk/upjs/ics/android/markdownr/PreviewFragment.java
+++ app/src/main/java/sk/upjs/ics/android/markdownr/PreviewFragment.java
@@ -6,20 +6,27 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 
 public class PreviewFragment extends Fragment {
+
+    public static final String URL_ENCODING = null;
+
+    private WebView previewWebView;
 
     public PreviewFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_preview, container, false);
+        View fragmentLayout = inflater.inflate(R.layout.fragment_preview, container, false);
+
+        this.previewWebView = (WebView) fragmentLayout.findViewById(R.id.previewWebView);
+        this.previewWebView.loadData("<i>No Markdown source</i>", "text/html; charset=UTF-8", URL_ENCODING);
+
+        return fragmentLayout;
     }
-
-
 }
