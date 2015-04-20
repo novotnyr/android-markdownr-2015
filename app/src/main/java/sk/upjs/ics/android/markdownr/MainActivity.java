8578bebfe492157b1d3b58194d903fce559667c0
novotnyr <robert.novotny@upjs.sk>
2015-04-20 16:55:12 +0200

    add resource for action bar items

--- app/src/main/java/sk/upjs/ics/android/markdownr/MainActivity.java
+++ app/src/main/java/sk/upjs/ics/android/markdownr/MainActivity.java
@@ -13,8 +13,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (isSinglePane()) {
-            showSourcePane();
+        if(savedInstanceState == null) {
+            if (isSinglePane()) {
+                showSourcePane();
+            }
         }
     }
 
@@ -39,16 +41,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
